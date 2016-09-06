6aa20d2ae82c91190339c6efb37432bb056058f9
Alex <glovy0@hotmail.com>
2016-09-06 12:44:10 +0200

    Added class for packet received event

---
+++ TcpSocketLib/TcpSocketLib/TcpSocketLib/PacketReceivedArgs.cs
@@ -0,0 +1,13 @@
+namespace TcpSocketLib
+{
+    public class PacketReceivedArgs
+        {
+            public byte[] Data { get; set; }
+            public int Length { get; set; }
+
+            public PacketReceivedArgs(byte[] data) {
+                this.Data = data;
+                this.Length = data.Length;
+            }
+        }
+}
