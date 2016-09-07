7fded059af55e02f5647ef7143ad4d2dca055146
Alex <glovy0@hotmail.com>
2016-09-07 12:54:18 +0200

    Changes

--- TcpSocketLib/TcpSocketLib/TcpSocketLib/UUID.cs
+++ TcpSocketLib/TcpSocketLib/TcpSocketLib/UUID.cs
@@ -7,24 +7,29 @@ namespace TcpSocketLib
     {
         private static List<Int32> idList = new List<Int32>();
         private static Random rng = new Random();
-        public static void PutBack(int Val) {
-            if (!idList.Contains(Val))
-                idList.Add(Val);
-        }
+
         private static int index = 0;
-        public static void InitializeUUIDS<T>(int start, int end) {
+
+        public static void PutBack(int value) {
+            if (!idList.Contains(value))
+                idList.Add(value);
+        }
+
+        public static void InitializeUUIDS(int start, int end, bool shuffle) {
             if (idList.Count < 1) {
                 for (int i = start; i < end; i++) {
                     idList.Add(i);
                 }
-                idList.Shuffle();
+                if(shuffle)
+                idList.Shuffle<int>();
             }
         }
-        public static int getRandomUUID() {
-            if (index < idList.Count)
+
+        public static int GetUniqueIdentifier() {
+            if (index <= idList.Count)
                 return idList[index++];
             else
-                throw new Exception("Out of UUIDS");
+                throw new Exception("Out of unique identifiers");
         }
     }
 }
