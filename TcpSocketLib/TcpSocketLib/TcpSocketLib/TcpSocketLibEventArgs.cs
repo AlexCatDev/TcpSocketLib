a2c25e32816055105d1b2688904dfdeaa3547b35
Alex <glovy0@hotmail.com>
2016-09-21 14:28:14 +0200

    Changes

---
+++ TcpSocketLib/TcpSocketLib/TcpSocketLib/TcpSocketLibEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcpSocketLib
+{
+    public class PacketReceivedArgs
+    {
+        public byte[] Data { get; set; }
+        public int Length { get; set; }
+
+        public PacketReceivedArgs(byte[] data) {
+            this.Data = data;
+            this.Length = data.Length;
+        }
+    }
+}
